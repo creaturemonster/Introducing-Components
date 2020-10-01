c3628afa5a3dafe2d88b33b8cce0bcbb55383fbd
Henggao Cai <creaturemonster@users.noreply.github.com>
2020-10-01 16:06:45 -0400

    Debugging

--- app.js
+++ app.js
@@ -1,46 +1,53 @@
-const app=Vue.createApp({
-    data(){
-      return {
-        detailsAreVisibleA:true,
-        detailsAreVisibleB:true,
-        friends:[
-            {id:'manuel',name:'Manuel Lopez', phone: '609-356-2573', email:'hgcai3456@gmail.com'}, {id:'julie', 'Julie Jones', phone:'609-356-2573', email:'hgcai3456@gmail.com' },],
-        ]
-      };
-    },
-    methods:{
-      toggleDetails(){
-        this.detailsAreVisible = !detailsAreVisible;
-      }
-    }
-});
-app.component('user-contact', {
-  template: `  <li v-for="friend in friends" :key="friend.id">
-      <h2>{{ friend.name }}</h2>
-      <button @click="toggleDetails">{{ detailsAreVisible ?  'Hide' : 'Show'}}Show Details</button>
-      <ul>
-        <li><strong>Phone:</strong>{{ friend.phone}}</li>
-        <li><strong>Email:</strong> {{ friend.email}}</li>
-      </ul>
-    </li>
-    <li>
-      <h2>Julie Jones</h2>
-      <button>Show Details</button>
-      <ul v-if="detailsAreVisible">
-        <li><strong>Phone:</strong>{{ friend.phone }}</li>
-        <li><strong>Email:</strong> {{ friend.email}}</li>
-      </ul>
-    </li>`,
-  data(){
-    return {detailsAreVisible: false,
-            friend:   {id:'manuel',name:'Manuel Lopez', phone: '609-356-2573', email:'hgcai3456@gmail.com'}
-          };
+const app = Vue.createApp({
+  data() {
+    return {
+      friends: [
+        {
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '01234 5678 991',
+          email: 'manuel@localhost.com',
+        },
+        {
+          id: 'julie',
+          name: 'Julie Jones',
+          phone: '09876 543 221',
+          email: 'julie@localhost.com',
+        },
+      ],
+    };
   },
-  methods:{
-    toggleDetails(){
-      this.detailsAreVisible = !this.detailsAreVisible;
-    }
-  }
-  }
 });
-app.mount("#app");
+
+app.component('friend-contact', {
+  template: `
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails()">
+      {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lorenzo',
+        phone: '01234 5678 991',
+        email: 'manuel@localhost.com',
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+});
+
+app.mount('#app');
